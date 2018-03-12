aa18cf7d8d01f1d1a72176f11aeead44a3d05b2d
EmuZONE <emuzone@outlook.com>
2018-03-12 17:59:26 +0100

    Add files via upload

---
+++ Source/MovieTown/MovieTown/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.AxVLCPlugin21 = New AxAXVLC.AxVLCPlugin2()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PlayBtn = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.PauseBtn = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.AxVLCPlugin21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.AxVLCPlugin21)
+        Me.Panel1.Location = New System.Drawing.Point(300, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(322, 225)
+        Me.Panel1.TabIndex = 0
+        '
+        'AxVLCPlugin21
+        '
+        Me.AxVLCPlugin21.Enabled = True
+        Me.AxVLCPlugin21.Location = New System.Drawing.Point(-2, -2)
+        Me.AxVLCPlugin21.Name = "AxVLCPlugin21"
+        Me.AxVLCPlugin21.OcxState = CType(resources.GetObject("AxVLCPlugin21.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxVLCPlugin21.Size = New System.Drawing.Size(322, 224)
+        Me.AxVLCPlugin21.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.PlayBtn)
+        Me.Panel2.Controls.Add(Me.Button7)
+        Me.Panel2.Controls.Add(Me.Button6)
+        Me.Panel2.Controls.Add(Me.Button5)
+        Me.Panel2.Controls.Add(Me.PauseBtn)
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Location = New System.Drawing.Point(333, 252)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(253, 34)
+        Me.Panel2.TabIndex = 1
+        '
+        'PlayBtn
+        '
+        Me.PlayBtn.BackgroundImage = Global.MovieTown.My.Resources.Resources.Play
+        Me.PlayBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PlayBtn.Location = New System.Drawing.Point(88, -1)
+        Me.PlayBtn.Name = "PlayBtn"
+        Me.PlayBtn.Size = New System.Drawing.Size(30, 30)
+        Me.PlayBtn.TabIndex = 7
+        Me.PlayBtn.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.BackgroundImage = CType(resources.GetObject("Button7.BackgroundImage"), System.Drawing.Image)
+        Me.Button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button7.Location = New System.Drawing.Point(149, -1)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(30, 30)
+        Me.Button7.TabIndex = 9
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.BackgroundImage = CType(resources.GetObject("Button6.BackgroundImage"), System.Drawing.Image)
+        Me.Button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button6.Location = New System.Drawing.Point(119, -1)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(30, 30)
+        Me.Button6.TabIndex = 8
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.BackgroundImage = CType(resources.GetObject("Button5.BackgroundImage"), System.Drawing.Image)
+        Me.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button5.Location = New System.Drawing.Point(185, -1)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(30, 30)
+        Me.Button5.TabIndex = 7
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'PauseBtn
+        '
+        Me.PauseBtn.BackgroundImage = Global.MovieTown.My.Resources.Resources.Pause
+        Me.PauseBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PauseBtn.Location = New System.Drawing.Point(88, -1)
+        Me.PauseBtn.Name = "PauseBtn"
+        Me.PauseBtn.Size = New System.Drawing.Size(30, 30)
+        Me.PauseBtn.TabIndex = 6
+        Me.PauseBtn.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackgroundImage = Global.MovieTown.My.Resources.Resources._Stop
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button3.Location = New System.Drawing.Point(58, -1)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(30, 30)
+        Me.Button3.TabIndex = 5
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImage = Global.MovieTown.My.Resources.Resources.Back
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.Location = New System.Drawing.Point(28, -1)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(30, 30)
+        Me.Button2.TabIndex = 4
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = Global.MovieTown.My.Resources.Resources.FastBack
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.Location = New System.Drawing.Point(-2, -1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(30, 30)
+        Me.Button1.TabIndex = 3
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Transparent
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel3.Controls.Add(Me.ComboBox1)
+        Me.Panel3.Location = New System.Drawing.Point(12, 12)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(279, 26)
+        Me.Panel3.TabIndex = 2
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Der Supercop", "Maniac Cop", "Maniac Cop 2", "Maniac Cop 3", "The Day After"})
+        Me.ComboBox1.Location = New System.Drawing.Point(-2, -2)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(277, 25)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Button4
+        '
+        Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button4.Location = New System.Drawing.Point(219, -1)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(30, 30)
+        Me.Button4.TabIndex = 10
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.BackgroundImage = CType(resources.GetObject("Button8.BackgroundImage"), System.Drawing.Image)
+        Me.Button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button8.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(216, 44)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 26)
+        Me.Button8.TabIndex = 3
+        Me.Button8.Text = "Abspielen"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.BackgroundImage = CType(resources.GetObject("Button9.BackgroundImage"), System.Drawing.Image)
+        Me.Button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button9.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(547, 388)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 26)
+        Me.Button9.TabIndex = 4
+        Me.Button9.Text = "Beenden"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.MovieTown.My.Resources.Resources.Silver_Matt
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(634, 426)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Movie Town: Advanced Online Movie Player"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.AxVLCPlugin21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents PauseBtn As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PlayBtn As Button
+    Friend WithEvents AxVLCPlugin21 As AxAXVLC.AxVLCPlugin2
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+End Class
---
+++ Source/MovieTown/MovieTown/Form1.vb
@@ -0,0 +1,264 @@
+﻿Public Class Form1
+    Private Sub PauseBtn_Click(sender As Object, e As EventArgs) Handles PauseBtn.Click
+        PauseBtn.Visible = False
+        PlayBtn.Visible = True
+        AxVLCPlugin21.playlist.togglePause()
+    End Sub
+
+    Private Sub PlayBtn_Click(sender As Object, e As EventArgs) Handles PlayBtn.Click
+        PauseBtn.Visible = True
+        PlayBtn.Visible = True
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        End
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Select Case ComboBox1.SelectedIndex
+            Case 0
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("https://archive.org/download/GermanMoviesVol2/Der%20Supercop.mp4")
+            Case 1
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("https://archive.org/download/GermanMoviesVol2/Maniac%20Cop.mp4")
+            Case 2
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("https://archive.org/download/GermanMoviesVol2/Maniac%20Cop%202.mp4")
+            Case 3
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("https://archive.org/download/GermanMoviesVol2/Maniac%20Cop%203.mp4")
+            Case 4
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("https://archive.org/download/GermanMoviesVol2/The%20Day%20After.mp4")
+            Case 5
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 6
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 7
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 8
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 9
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 10
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 11
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 12
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 13
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 14
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 15
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 16
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 17
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 18
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 19
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 20
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 21
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 22
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 23
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 24
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 25
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 26
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 27
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 28
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 29
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 30
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 31
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 32
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 33
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 34
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 35
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 36
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 37
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 38
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 39
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 40
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 41
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 42
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 43
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 44
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 45
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 46
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 47
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 48
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 49
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 50
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 51
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 52
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 53
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 54
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 55
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 56
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 57
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 58
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 59
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 60
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 61
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 62
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 63
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 64
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 65
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 66
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 67
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 68
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 69
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 70
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+            Case 71
+                AxVLCPlugin21.playlist.clear()
+                AxVLCPlugin21.playlist.add("")
+        End Select
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        PlayBtn.Visible = False
+        PauseBtn.Visible = True
+        AxVLCPlugin21.playlist.next()
+        AxVLCPlugin21.playlist.play()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        AxVLCPlugin21.video.fullscreen = True
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        AxVLCPlugin21.playlist.next()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        AxVLCPlugin21.video.crop = "16:9"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        AxVLCPlugin21.playlist.stop()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        AxVLCPlugin21.playlist.prev()
+    End Sub
+End Class
